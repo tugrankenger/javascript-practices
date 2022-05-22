2a1fcb28e6226eb00efa309151665ce2d5e254b6
Tugran Kenger <turankenger@hotmail.com.tr>
2022-05-22 19:29:53 +0300

    put and delete requests

--- fetch-and-promise-http-requests/app.js
+++ fetch-and-promise-http-requests/app.js
@@ -15,13 +15,39 @@ class Request{
         return new Promise((resolve,reject)=>{
             fetch(url,{
                 method:'POST',
-                body: JSON.stringify({data}),
+                body: JSON.stringify(data),
                 headers:{
                     'Content-type':'application/json; charset= UTF-8',
                 },
             })
             .then(response => response.json())
             .then(data => resolve(data))
+            .catch(err => reject(err))
+        })
+    }
+
+    put(url,data){
+        return new Promise((resolve,reject)=>{
+            fetch(url,{
+                method: 'PUT',
+                body: JSON.stringify(data),
+                headers:{
+                    'Content-type':'application/json; charset= UTF-8',
+                },
+            })
+            .then(response => response.json())
+            .then(data => resolve(data))
+            .catch(err => reject(err))
+        })
+    }
+
+    delete(url){
+        return new Promise((resolve,reject)=>{
+            fetch(url,{
+                method: 'DELETE',
+            })
+            .then(response => response.json())
+            .then(afterDeleted => resolve('Deleted successfully'))
             .catch(err => reject(err))
         })
     }
@@ -37,4 +63,12 @@ request.get('https://jsonplaceholder.typicode.com/albums')
 
 request.post('https://jsonplaceholder.typicode.com/posts',{"userId":333,"title":"tugrankenger"})
 .then(addedItem => console.log(addedItem))
+.catch(err => console.error(err))
+
+request.put('https://jsonplaceholder.typicode.com/posts/1',{body:"tugrankenger",userId:3334})
+.then(updatedData => console.log(updatedData))
+.catch(err => console.error(err))
+
+request.delete('https://jsonplaceholder.typicode.com/albums/1')
+.then(resultText => console.log(resultText))
 .catch(err => console.error(err))
