75692277ae4fd4bbe250675405bae343e1c06f10
Tugran Kenger <turankenger@hotmail.com.tr>
2022-05-22 18:59:17 +0300

    post request

--- fetch-and-promise-http-requests/app.js
+++ fetch-and-promise-http-requests/app.js
@@ -10,6 +10,21 @@ class Request{
             .catch(err => reject(err))
         })
     }
+
+    post(url, data){
+        return new Promise((resolve,reject)=>{
+            fetch(url,{
+                method:'POST',
+                body: JSON.stringify({data}),
+                headers:{
+                    'Content-type':'application/json; charset= UTF-8',
+                },
+            })
+            .then(response => response.json())
+            .then(data => resolve(data))
+            .catch(err => reject(err))
+        })
+    }
 }
 
 const request = new Request();
@@ -19,3 +34,7 @@ request.get('https://jsonplaceholder.typicode.com/albums')
 .catch(err => console.error(err))
 
 //console.log(albums); // asenkron
+
+request.post('https://jsonplaceholder.typicode.com/posts',{"userId":333,"title":"tugrankenger"})
+.then(addedItem => console.log(addedItem))
+.catch(err => console.error(err))
