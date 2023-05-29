d0e72a9f0a98dbfdd44e0d620afd86449e95fdec
tugrankenger <turankenger@hotmail.com.tr>
2023-05-29 22:30:22 +0300

    create function type and array type

--- typescript/index.ts
+++ typescript/index.ts
@@ -24,10 +24,31 @@ let actor2: ActorInterface = {
 
 type ActorType = {
   name: string;
+  character: string;
   age?: number; // optional chaining
 };
 
 let actor3: ActorType = {
   name: 'Desmond Hume',
+  character: 'hugo',
   // age: 111,
 };
+
+// interface SumDigit {
+//   (x:number,y:number):number
+// }
+
+// create function type
+
+type Sum = (x: number, y: number) => number;
+
+const sum: Sum = (x, y) => {
+  return x + y;
+};
+
+const result = sum(2, 3);
+
+const movies: Array<ActorType> = [
+  { name: 'lost', character: 'hugo', age: 33 },
+  { name: 'breaking bad', character: 'walter white', age: 111 },
+];
