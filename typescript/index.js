46e2040a44f76a8264a204abd074a3e2e6bdbd38
tugrankenger <turankenger@hotmail.com.tr>
2023-05-30 09:47:18 +0300

    create generic type

---
+++ typescript/index.js
@@ -0,0 +1,27 @@
+var actor = {
+    name: "Terry O'Quinn",
+    character: 'John Locke',
+    age: 13,
+};
+var actor2 = {
+    name: 'Elizabeth Mitchell',
+    character: 'Juliet Burke',
+    age: 13,
+};
+var actor3 = {
+    name: 'Desmond Hume',
+    character: 'hugo',
+    // age: 111,
+};
+var sum = function (x, y) {
+    return x + y;
+};
+var result = sum(2, 3);
+var movies = [
+    { name: 'lost', character: 'hugo', age: 33 },
+    { name: 'breaking bad', character: 'walter white', age: 111 },
+];
+var getActor = {
+    name: "tugran",
+    age: 11,
+};
--- typescript/index.ts
+++ typescript/index.ts
@@ -52,3 +52,24 @@ const movies: Array<ActorType> = [
   { name: 'lost', character: 'hugo', age: 33 },
   { name: 'breaking bad', character: 'walter white', age: 111 },
 ];
+
+// union types:
+type name = number | string
+type age = number
+
+//literal types:
+
+// type name2 = "tugran"
+// type age2 = 13
+
+// generics types
+
+interface ActorGen<name, age> {
+  name: name;
+  age?: age;
+}
+
+let getActor: ActorGen<name, age> ={
+  name:"tugran",
+  age: 11,
+}
