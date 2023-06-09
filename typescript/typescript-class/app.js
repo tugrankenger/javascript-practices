1d9af0cd947cb66142ce0190bdad310abebdb439
tugrankenger <turankenger@hotmail.com.tr>
2023-06-09 11:49:49 +0300

    create person class using constructor

--- typescript/typescript-class/app.js
+++ typescript/typescript-class/app.js
@@ -9,3 +9,17 @@ Person.prototype.getFullName = function () {
 var person = new Person('01.01.3333', 'john', 'locke');
 console.log('person normal: ', person);
 console.log('person proto: ', person.getFullName());
+// create person class using constructor (syntatic sugar)
+var Person2 = /** @class */ (function () {
+    function Person2(date, firstName, lastName) {
+        this.date = date;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    Person2.prototype.getFullName = function () {
+        return "".concat(this.firstName, " ").concat(this.lastName);
+    };
+    return Person2;
+}());
+var personConst = new Person2('01.02.2333', 'tugran', 'kenger');
+console.log('person constructor: ', personConst.getFullName());
--- typescript/typescript-class/app.ts
+++ typescript/typescript-class/app.ts
@@ -13,3 +13,23 @@ let person = new Person('01.01.3333', 'john', 'locke');
 console.log('person normal: ', person);
 console.log('person proto: ', person.getFullName());
 
+// create person class using constructor (syntatic sugar)
+class Person2 {
+  date:string;
+  firstName :string;
+  lastName :string;
+
+  constructor(date :string, firstName:string, lastName:string) {
+    this.date = date;
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  getFullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+let personConst = new Person2('01.02.2333', 'tugran', 'kenger');
+
+console.log('person constructor: ', personConst.getFullName());
