02bb4c221c98d96a6c9b5854d72f7c31fd164270
tugrankenger <turankenger@hotmail.com.tr>
2023-07-10 18:52:33 +0300

    delete unnecessary files and create functional component

--- typescript/todo-list/src/App.tsx
+++ typescript/todo-list/src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC } from 'react';
 import './App.css';
 
-function App() {
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='header'>
+        <div className='inputContainer'>
+          <input type='text' placeholder='Enter task...' />
+          <input type='number' placeholder='Enter deadline' />
+        </div>
+        <button>Add Todo</button>
+      </div>
+      <div className='todoList'></div>
     </div>
   );
-}
+};
 
 export default App;
