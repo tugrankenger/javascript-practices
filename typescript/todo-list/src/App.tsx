0317cdc125bd1b37af536ad10d3f157e07282e79
tugrankenger <turankenger@hotmail.com.tr>
2023-07-11 16:41:38 +0300

    create state and add function

--- typescript/todo-list/src/App.tsx
+++ typescript/todo-list/src/App.tsx
@@ -1,15 +1,41 @@
-import React, { FC } from 'react';
+import React, { FC, ChangeEvent, useState } from 'react';
 import './App.css';
 
 const App: FC = () => {
+  const [task, setTask] = useState<string>('');
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todoList, setTodoList] = useState([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.name === 'task') {
+      setTask(event.target.value);
+    } else {
+      setDeadline(Number(event.target.value));
+    }
+  };
+
+  const addTask = () => {
+    console.log(task, deadline);
+  };
+  
   return (
     <div className='App'>
       <div className='header'>
         <div className='inputContainer'>
-          <input type='text' placeholder='Enter task...' />
-          <input type='number' placeholder='Enter deadline' />
+          <input
+            type='text'
+            placeholder='Enter task...'
+            name='task'
+            onChange={handleChange}
+          />
+          <input
+            type='number'
+            placeholder='Enter deadline'
+            name='deadline'
+            onChange={handleChange}
+          />
         </div>
-        <button>Add Todo</button>
+        <button onClick={addTask}>Add Todo</button>
       </div>
       <div className='todoList'></div>
     </div>
