517a271a87be680816ad6c985c1793aa43346793
tugrankenger <turankenger@hotmail.com.tr>
2023-07-12 15:47:12 +0300

    create interface for todolist

--- typescript/todo-list/src/App.tsx
+++ typescript/todo-list/src/App.tsx
@@ -1,10 +1,11 @@
 import React, { FC, ChangeEvent, useState } from 'react';
 import './App.css';
+import { ITask } from './interfaces';
 
 const App: FC = () => {
   const [task, setTask] = useState<string>('');
   const [deadline, setDeadline] = useState<number>(0);
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState<ITask[]>([{taskName:"test1",deadline:1}]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     if (event.target.name === 'task') {
@@ -16,8 +17,13 @@ const App: FC = () => {
 
   const addTask = () => {
     console.log(task, deadline);
+    const newTask = {taskName:task, deadline:deadline}
+    setTodoList([...todoList, newTask])
+    setTask("")
+    setDeadline(0)
+    console.log("tasks: ", todoList)
   };
-  
+
   return (
     <div className='App'>
       <div className='header'>
@@ -26,12 +32,14 @@ const App: FC = () => {
             type='text'
             placeholder='Enter task...'
             name='task'
+            value={task}
             onChange={handleChange}
           />
           <input
             type='number'
             placeholder='Enter deadline'
             name='deadline'
+            value={deadline}
             onChange={handleChange}
           />
         </div>
